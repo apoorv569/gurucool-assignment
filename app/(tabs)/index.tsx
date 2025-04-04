d34049f1c2eda6df66fceefb3eb1732260767be3
apoorv569 <apoorvs569@gmail.com>
2025-04-04 12:46:17 +0530

    Add SafeAreaView to Home tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import Card from "@/components/Card";
 import { settingsContext } from "@/context/SettingsContext";
 import { userContext } from "@/context/UserContext";
 import { useContext, useEffect, useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
     const { isDarkMode, quotesToFetch } = useContext(settingsContext);
@@ -19,25 +19,30 @@ export default function Home() {
     }, [quotesToFetch])
 
     return (
-        <View style={[styles.container, { backgroundColor: isDarkMode ? "#5A2D6D" : "#F8F0FF" }]}>
-            <Text style={[styles.text, { color: isDarkMode ? "#D8BFD8" : "#4B0082" }]}>
-                Welcome {username}, here are some cool quotes.
-            </Text>
-            <FlatList
-                data={items}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={
-                    ({ item }) =>
-                        <Card key={item.id} content={item.quote} subtitle={item.author} />
-                }
-                contentContainerStyle={{ paddingBottom: 16 }}
-                ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
-            />
-        </View>
+        <SafeAreaView style={[styles.safeContainer, { backgroundColor: isDarkMode ? "#5A2D6D" : "#F8F0FF" }]}>
+            <View style={[styles.container, { backgroundColor: isDarkMode ? "#5A2D6D" : "#F8F0FF" }]}>
+                <Text style={[styles.text, { color: isDarkMode ? "#D8BFD8" : "#4B0082" }]}>
+                    Welcome {username}, here are some cool quotes.
+                </Text>
+                <FlatList
+                    data={items}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={
+                        ({ item }) =>
+                            <Card key={item.id} content={item.quote} subtitle={item.author} />
+                    }
+                    contentContainerStyle={{ paddingBottom: 16 }}
+                    ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+                />
+            </View>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    safeContainer: {
+        flex: 1,
+    },
     container: {
         flex: 1,
         alignItems: 'center',
